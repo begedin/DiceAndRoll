0bf1a0ee3d037d443b0d52b2c1862e957c356aaf
Nikola Begedin <begedinnikola@gmail.com>
2014-10-10 09:08:10 +0200

    Fix issue with resize event on mobile devices

--- scripts/inc/states/boot.js
+++ scripts/inc/states/boot.js
@@ -50,7 +50,7 @@ define(['Phaser'], function (Phaser) {
                 this.scale.pageAlignHorizontally = true;
                 this.scale.pageAlignVertically = true;
                 this.scale.forceOrientation(true, false);
-                this.scale.hasResized.add(this.gameResized, this);
+                this.scale.onResize = this.gameResized.bind(this);
                 this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
                 this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
                 this.scale.setScreenSize(true);
